4cd64621fcad7ec997a1ba76525a04724a436b5f
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2022-10-11 15:12:09 -0500

    Created linked list interface

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,50 @@
-/*
- * your comment header here
- */
+/*********************
+Name: Kevin Bryniak
+Assignment: Linked List
+A example implementation of a linked list.
+**********************/
+#include "linkedlist.h"
+
+LinkedList::LinkedList()
+{
+  head = NULL;
+}
+
+LinkedList::~LinkedList()
+{
+  clearList();
+}
+
+bool LinkedList::addNode(int id, string *data)
+{
+  return true;
+}
+
+bool LinkedList::deleteNode(int id)
+{
+  return true;
+}
+
+bool LinkedList::getNode(int, Data *)
+{
+  return true;
+}
+
+void LinkedList::printList(bool reverse)
+{
+}
+
+int LinkedList::getCount()
+{
+  return 0;
+}
+
+bool LinkedList::clearList()
+{
+  return true;
+}
+
+bool LinkedList::exists(int id)
+{
+  return true;
+}
--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,36 @@
-/*
- * your comment header here
- */
+/*********************
+Name: Kevin Bryniak
+Assignment: Linked List
+A example implementation of a linked list.
+**********************/
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "data.h"
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class LinkedList
+{
+private:
+  Node *head;
+
+public:
+  LinkedList();
+  ~LinkedList();
+
+  bool addNode(int, string *);
+  bool deleteNode(int);
+  bool getNode(int, Data *);
+  void printList(bool = false);
+  int getCount();
+  bool clearList();
+  bool exists(int);
+};
+
+#endif
